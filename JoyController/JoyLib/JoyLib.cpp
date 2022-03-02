4188c7c0dfeb8e70a1033e95bef22f460b38c072
AJ <aj.sampathk@gmail.com>
2022-03-02 23:34:14 +0000

    JoyLib file write update

--- JoyController/JoyLib/JoyLib.cpp
+++ JoyController/JoyLib/JoyLib.cpp
@@ -1,6 +1,5 @@
 #include "JoyLib.h"
-#include <iostream>
-#include <fstream>
+
 
 //define all the functions here
 uint8_t JoyLib::get_buttons(){
@@ -11,46 +10,51 @@ uint16_t JoyLib::get_axis(){
     return (out_bytes[0]<<8|out_bytes[1]);
 }
 
+bool JoyLib::begin(char* device){
+  fout.open(device, std::ios::binary | std::ios::out);
+  write();
+
+}
+
 bool JoyLib::press(int8_t button){
-    //TODO:update JoyLib::out_bytes[2] to press a button and call write
+    //to press a button and call write
     out_bytes[2]= out_bytes[2]|(1<<(button-1));
     write();
     return true;
 }
 
 bool JoyLib::release(int8_t button){
-    //TODO:update JoyLib::out_bytes[2] to release one button and call write
+    // to release one button and call write
     out_bytes[2]= out_bytes[2]&(~(1<<(button-1)));
     write();
     return true;
 }
 
 bool JoyLib::releaseAll(){
-    //TODO:update JoyLib::out_bytes[2] to release all buttons and call write
+    //to release all buttons and call write
     out_bytes[2]=0;
     write();
     return true;
 }
 
 bool JoyLib::xAxis(int8_t value){
-    //TODO:update JoyLib::out_bytes[0] and call write
+    //set X-Axis and call write
+    out_bytes[0]=value;
     write();
 }
 
 bool JoyLib::yAxis(int8_t value){
-    //TODO:update JoyLib::out_bytes[1] and call write
+    //set Y-Axis and call write
+    out_bytes[1]=value;
     write();
 }
 
 
 
 bool JoyLib::write(){
-    //TODO: write bytes in JoyLib::out_bytes to /dev/hidg0
-    std::ofstream fout;
-    fout.open("try.txt", std::ios::binary | std::ios::out);
 
     fout.write((char*) &out_bytes, sizeof(out_bytes));
+    fout.flush();
 
-    fout.close();
     return true;
 }
--- JoyController/JoyLib/JoyLib.h
+++ JoyController/JoyLib/JoyLib.h
@@ -1,11 +1,14 @@
 #ifndef _JOYLIB_H
 #define _JOYLIB_H
 #include <cstdint>
+#include <iostream>
+#include <fstream>
 
 // Declare and include all the neccessary libs here 
 
 class JoyLib {
     public:
+        bool begin(char* device);
         bool press(int8_t button);
         bool release(int8_t button);
         bool releaseAll();
@@ -14,7 +17,8 @@ class JoyLib {
         uint8_t get_buttons();
         uint16_t get_axis();
     private:
-        uint8_t out_bytes[3];
+        std::ofstream fout;
+        uint8_t out_bytes[3] = {0,0,0};
         bool write();
 
 
