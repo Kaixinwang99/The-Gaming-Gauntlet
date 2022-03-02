c3b250dec191af09883fb544a156812955f07b76
AJ <aj.sampathk@gmail.com>
2022-03-02 17:40:09 +0000

    bug explore

--- rpi_ads1115-main/example/ads1115_printer.cpp
+++ rpi_ads1115-main/example/ads1115_printer.cpp
@@ -29,7 +29,7 @@ class ADS1115Printer : public ADS1115rpi {
 		virtual void hasSample(float v) {
 			latest_values[current_channel]=v;
 			printf("[0]:%e|[1]]:%e|[2]:%e|[3]:%e|\n",latest_values[0],latest_values[1],latest_values[2],latest_values[3]);
-			printf("------[Channel]:\n",current_channel);
+			printf("------[Channel]:%d\n",current_channel);
 			current_channel = (current_channel+1)%4;
 			setChannel((ADS1115settings::Input)current_channel);
 		}
