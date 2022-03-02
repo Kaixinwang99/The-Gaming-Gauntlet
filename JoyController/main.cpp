52ee895a1abd8b5e1204e3d8319d7e322a73ac90
AJ <aj.sampathk@gmail.com>
2022-03-02 23:49:08 +0000

    JoyLib dev test

--- JoyController/main.cpp
+++ JoyController/main.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 #include <JoyLib.h>
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>         // std::chrono::seconds
 
 int main(int, char**) {
     JoyLib joy;
-    const char* device = "hidg0";
+    const char* device = "/dev/hidg0";
     std::cout<<"\nStarting joy with device:"<<device<<"\n";
     joy.begin(device);
-
-    // std::cout<<"\nPressing button 8"<<"\n";
-    // joy.press(8);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::cout<<"\nPressing button 8"<<"\n";
+    joy.press(8);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
     // printf("[BUTTONS][HEX]:0x%02x\n",joy.get_buttons());
 
-    // std::cout<<"\nReleasing button 8"<<"\n";
-    // joy.release(8);
+    std::cout<<"\nReleasing button 8"<<"\n";
+    joy.release(8);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
     // printf("[BUTTONS][HEX]:0x%02x\n",joy.get_buttons());
 
 
-    // std::cout<<"\nPressing buttons 1,2,3"<<"\n";
-    // joy.press(1);
-    // joy.press(2);
-    // joy.press(3);
+    std::cout<<"\nPressing buttons 1,2,3"<<"\n";
+    joy.press(1);
+    joy.press(2);
+    joy.press(3);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
     // printf("[BUTTONS][HEX]:0x%02x\n",joy.get_buttons());
 
-    // std::cout<<"\nReleasing all buttons"<<"\n";
-    // joy.releaseAll();
+    std::cout<<"\nReleasing all buttons"<<"\n";
+    joy.releaseAll();
     // printf("[BUTTONS][HEX]:0x%02x\n",joy.get_buttons());
 
     std::cout<<"\nExiting..\n";
