0b5096b35018d78bc2d540b2b71f15a22d6a5b4f
AJ <aj.sampathk@gmail.com>
2022-03-13 19:00:21 +0000

    debug

--- ICM20948/rpi_ICM20948/icm20948.cpp
+++ ICM20948/rpi_ICM20948/icm20948.cpp
@@ -224,7 +224,7 @@ icm20948::icm20948(uint8_t addr){
 
 
     if(read(ICM20948_WHO_AM_I) != CHIP_ID){
-        fprintf(stderr,"Cannot find ICM20948");
+        fprintf(stderr,"Cannot find ICM20948:%02x=/=%02x",read(ICM20948_WHO_AM_I),CHIP_ID);
         throw "Cannot find ICM20948";
     }
     bank(0);
@@ -249,7 +249,7 @@ icm20948::icm20948(uint8_t addr){
     write(ICM20948_I2C_MST_DELAY_CTRL, 0x01);
 
     if(mag_read(AK09916_WIA) != AK09916_CHIP_ID){
-        fprintf(stderr,"Cannot find magnetometer:%d=/=%d",mag_read(AK09916_WIA),AK09916_CHIP_ID);
+        fprintf(stderr,"Cannot find magnetometer:%02x=/=%02x",mag_read(AK09916_WIA),AK09916_CHIP_ID);
         throw "unable to find Magnetometer chip";
     }
 
