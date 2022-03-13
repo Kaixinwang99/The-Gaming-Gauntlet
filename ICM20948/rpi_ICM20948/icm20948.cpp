6797adea59373aad77668d8a9414d79815650890
AJ <aj.sampathk@gmail.com>
2022-03-13 19:54:43 +0000

    remove debug messages

--- ICM20948/rpi_ICM20948/icm20948.cpp
+++ ICM20948/rpi_ICM20948/icm20948.cpp
@@ -35,7 +35,6 @@ void icm20948::write(unsigned int reg,unsigned int value){
 
 void icm20948::trigger_mag_io(){
     uint8_t user = read(ICM20948_USER_CTRL);
-    fprintf(stdout,"%02x",user);
     write(ICM20948_USER_CTRL, user | 0x20);
     usleep(5000);
     write(ICM20948_USER_CTRL, user);
